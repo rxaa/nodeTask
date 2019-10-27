e02eb5672660a4f15fef0a7c747d80a4f5302bd3
rxaa <zyxdde@gmail.com>
2019-10-27 13:00:40 +0800

    task

--- task.js
+++ task.js
@@ -8,11 +8,10 @@ const dfv = require("dfv");
 //每日凌晨3点定时任务
 const task = dfv.dfvTime.dailyEvent(3, 0, e => {
     let date = new Date();
-    if (date.getDate() == 1) {
-        sh.exec("pm2 stop app");
+     if (date.getDate() == 1 || date.getDate() == 15) {
+        sh.exec("service nginx stop");
         sh.exec("certbot renew");
-        sh.exec("pm2 start app");
-        //sh.cd("/usr/local/blog/");
+        sh.exec("service nginx start");
     }
 });
 
